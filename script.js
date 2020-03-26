08dbf7ed9180ea4b33b982e77f4d1172428b766c
robynp108 <robynp108@gmail.com>
2020-03-25 22:47:23 -0500

    local storage code

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 var d = new Date();
 document.getElementById("currentDay").textContent = d;
+
+
+var inputIDs = ["#input-9", "#input-10", "#input-11", "#input-12", "#input-1", "#input-2", "#input-3", "#input-4", "#input-5"];
+
+for (var i = 0; i < inputIDs.length; i++) {
+    var inputID = inputIDs[i];
+    $(inputID).val(localStorage.getItem(inputID));
+}
+
+//event handler for submit button with storage of input
+
+$(".saveButton").on("click", function () {
+
+    for (var i = 0; i < inputIDs.length; i++) {
+        var inputID = inputIDs[i];
+        var inputValue = $(inputID).val();
+        localStorage.setItem(inputID, inputValue);
+    }
+
+})
+
